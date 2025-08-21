326fca486fde4c8d0ea36eaaec1dada53e87e6d9
olaore66 <olaore66@gmail.com>
2025-08-21 13:27:11 +0100

    Holify Initial commit

---
+++ src/main/java/Holify/holify/HolifyApplication.java
@@ -0,0 +1,13 @@
+package Holify.holify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Holify/holify/HolifyApplicationTests.java
@@ -0,0 +1,13 @@
+package Holify.holify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
